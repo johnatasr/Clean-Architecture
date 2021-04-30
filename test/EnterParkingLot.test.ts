495afdc441e9ed3a2feed2a1c320e19aaacf245c
Johnatas <johnatasr@hotmail.com>
2021-04-30 16:55:06 -0300

    14022

--- test/EnterParkingLot.test.ts
+++ test/EnterParkingLot.test.ts
@@ -1,14 +1,26 @@
 import EnterParkingLot from "../src/core/useCases/EnterParkingLot";
+import GetParkingLot from "../src/core/useCases/GetParkingLot";
 import ParkingLotRepoMemory from "../src/infra/ParkingLotRepoMemory"
 
 test("Shoud enter parking lot", async function() {
     const parkingLotRepoMemory = new ParkingLotRepoMemory();
-    const enterParkingLot = new EnterParkingLot(parkingLotRepoMemory);
-    const parkingLot = await enterParkingLot.execute(
+    const enterParkingLotUseCase = new EnterParkingLot(parkingLotRepoMemory);
+    const parkingLot = await enterParkingLotUseCase.execute(
         "shopping",
         "PPP-4578",
         new Date("2021-04-30T12:21:50")
         );
+    const getParkingLotUseCase = new GetParkingLot(parkingLotRepoMemory);
+    const parkingLotBeforEnter = getParkingLotUseCase.execute("shopping");
+    
+    expect(parkingLotBeforEnter.occSpaces).toBe(0)
+    
+    await enterParkingLotUseCase.execute(
+        "supermarket",
+        "MHH-1313",
+        new Date("2021-04-30T15:54:20")
+    )
+    
     expect(parkingLot.code).toBe("shopping")
 
 })
